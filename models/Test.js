b6830547e4acd4195c9b963a79b0789f35016e1f
ol222hf <ol222hf@student.lnu.se>
2020-04-23 21:24:47 +0200

    test schema

---
+++ models/Test.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+// Create a schema.
+const testSchema = new mongoose.Schema(
+	{
+		test: {
+			text: String
+		}
+	},
+	{
+		timestamps: true,
+		versionKey: false
+	}
+)
+
+// Create a model using the schema.
+const Test = mongoose.model('Test', testSchema)
+
+// Exports.
+module.exports = Test
