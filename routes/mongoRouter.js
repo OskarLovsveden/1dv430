9fccbe7234a0803c402901a86b26867ea376eabb
ol222hf <ol222hf@student.lnu.se>
2020-04-26 18:11:18 +0200

    adding route for removing testdata

--- routes/mongoRouter.js
+++ routes/mongoRouter.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/mongoController')
 
 router.get('/test-get', controller.testGet)
 router.post('/test-post', controller.testPost)
+router.post('/test-remove', controller.testRemoveAll)
 
 module.exports = router
