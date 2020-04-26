142216d6470df3fc7b5e1325c0fc1496a4bbb833
ol222hf <ol222hf@student.lnu.se>
2020-04-26 12:06:47 +0200

    renaming Demo to Layout

---
+++ client/src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// Components
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import TestIGDB from './TestIGDB'
+
+export const Layout = () => {
+	return (
+		<>
+			<Router>
+				<div>
+					<Nav />
+					{/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+					<Switch>
+						<Route path="/testigdb" component={TestIGDB} />
+						<Route path="/" component={Home} />
+					</Switch>
+				</div>
+			</Router>
+			<Footer />
+		</>
+	)
+}
+
+export default Layout
