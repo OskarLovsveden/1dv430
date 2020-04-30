3b7295da4b9eaa8e9946d5fabaaf4e3da2cb5639
ol222hf <ol222hf@student.lnu.se>
2020-04-30 18:01:02 +0200

    Adding "default" route

--- routes/igdbRouter.js
+++ routes/igdbRouter.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const controller = require('../controllers/igdbController')
 
+router.get('/', (req, res) => res.sendStatus(200))
 router.get('/test', controller.test)
 
 module.exports = router
