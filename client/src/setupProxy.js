0c81ed7155397ef1c23dae1db1916ec832bcedd0
ol222hf <ol222hf@student.lnu.se>
2020-05-02 21:33:45 +0200

    Development Proxy

---
+++ client/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+	app.use(
+		'/games',
+		createProxyMiddleware({
+			target: process.env.REACT_APP_IGDB_URL,
+			changeOrigin: true
+		})
+	)
+	// app.use(proxy('/games', { target: process.env.REACT_APP_IGDB_URL }))
+}
