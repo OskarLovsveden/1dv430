30d852524792ead57b491a73945d8816a9afcbdc
ol222hf <ol222hf@student.lnu.se>
2020-05-02 21:34:29 +0200

    Displaying SearchBar in Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// Components
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
-	return <h2>Home</h2>
+	const [query, setQuery] = useState(null)
+
+	const submitToQuery = searchInput => {
+		console.log('submitToQuery', searchInput)
+		setQuery(searchInput)
+	}
+
+	return (
+		<div>
+			<SearchBar placeholder="Search for a game..."></SearchBar>
+			{/* <SearchBar
+				submit={submitToQuery}
+				placeholder="Search for a game..."
+			></SearchBar> */}
+			<h1>{query}</h1>
+		</div>
+	)
 }
 
+// Exports
 export default Home
