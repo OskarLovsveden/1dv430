dadada1e5d48d3fb61914614b93a9ee3ba942bd5
ol222hf <ol222hf@student.lnu.se>
2020-05-02 21:32:10 +0200

    Adding Proptypes and search-functionality

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,26 +1,49 @@
 import React, { useState } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+
+// Apicalypse
+import apicalypseHelpers from '../apicalypseHelpers'
 
 const SearchBar = ({ submit, placeholder }) => {
-	const [state, setState] = useState()
+	const [input, setInput] = useState(null)
 
-	const onSubmit = event => {
+	const onSubmitForm = async event => {
 		event.preventDefault()
-
-		console.log('Submitting Input: ', state.input)
-		submit(state.input)
+		const data = await apicalypseHelpers.getGames(input)
+		console.log(data)
+		// console.log(event.target)
+		// console.log('Submitting Input: ', input)
+		// submit(input)
 	}
 
-	const onWrite = event => {
+	const onWriteInput = event => {
 		console.log('Writing Input: ', event.target.value)
-		setState({ input: event.target.value })
+		setInput(event.target.value)
+	}
+
+	const handleKeypress = event => {
+		if (event.key === 'Enter') {
+			event.target.value = ''
+		}
 	}
 
 	return (
-		<form onSubmit={onSubmit}>
-			<input type="text" onChange={onWrite} placeholder={placeholder}></input>
+		<form onSubmit={onSubmitForm}>
+			<input
+				type="text"
+				onKeyPress={handleKeypress}
+				onChange={onWriteInput}
+				placeholder={placeholder}
+			></input>
 		</form>
 	)
 }
 
+// Proptypes
+SearchBar.propTypes = {
+	submit: PropTypes.func,
+	placeholder: PropTypes.string
+}
+
+// Exports
 export default SearchBar
