4a2466d8b4d98d61c79923d2b6cc0e28d781826f
ol222hf <ol222hf@student.lnu.se>
2020-05-04 00:39:34 +0200

    removing unused exports

--- config/dotenv.js
+++ config/dotenv.js
@@ -3,7 +3,5 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-	PORT: process.env.PORT,
-	API_URL: process.env.IGDB_URL,
-	API_KEY: process.env.IGDB_KEY
+	PORT: process.env.PORT
 }
