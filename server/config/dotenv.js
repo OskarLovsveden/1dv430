2cc0d78323be2c7ac13d1834e5d9cc716bab8b41
ol222hf <ol222hf@student.lnu.se>
2020-05-06 19:00:39 +0200

    MongDB and IGDB env_variables added to config

--- server/config/dotenv.js
+++ server/config/dotenv.js
@@ -3,5 +3,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-	PORT: process.env.PORT
+	PORT: process.env.PORT,
+	DB_STRING: process.env.DB_CONNECTION_STRING,
+	IGDB_URL: process.env.IGDB_URL,
+	IGDB_KEY: process.env.IGDB_KEY
 }
