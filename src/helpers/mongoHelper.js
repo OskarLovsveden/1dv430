c7881b3ff0a7e5ac7fa10fd75a26dd547ba69548
ol222hf <ol222hf@student.lnu.se>
2020-05-11 11:32:18 +0200

    adding helper function to get specific gamelist

--- src/helpers/mongoHelper.js
+++ src/helpers/mongoHelper.js
@@ -1,12 +1,16 @@
 import axios from 'axios'
 
 const mongoHelper = {
-	newList: async (req, res, name) => {
-		const response = await axios.post('mongo/lists/new', { name: name })
+	newList: async (req, res) => {
+		const response = await axios.post('mongo/lists/new')
 		console.log(response)
 	},
 	getLists: async (req, res) => {
 		const response = await axios('/mongo/lists')
+		return response.data
+	},
+	getList: async id => {
+		const response = await axios(`/mongo/list/${id}`)
 		return response.data
 	}
 }
