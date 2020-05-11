4c83a6c2991567023d12c52b84a2383411b2a7e8
ol222hf <ol222hf@student.lnu.se>
2020-05-11 12:24:29 +0200

    handling data and rendering it

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,45 @@
-import React, { useEffect } from 'react'
+import moment from 'moment'
+
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import mongoHelper from '../helpers/mongoHelper'
 const { getList } = mongoHelper
 
 const List = () => {
-	let { id } = useParams()
+	const [name, setName] = useState('')
+	// const [ID, setID] = useState('')
+	const [games, setGames] = useState([])
+	const [createdAt, setCreatedAt] = useState('')
+
+	const { id } = useParams()
+
 	useEffect(() => {
 		const getListOnRender = async () => {
 			const list = await getList(id)
-			console.log(list)
+			setName(list.name)
+			// setID(list._id)
+			setGames(list.gamesArray)
+			setCreatedAt(moment(list.createdAt).format('YYYY-MM-DD'))
 		}
 		getListOnRender()
-	})
-	return <div>{id}</div>
+	}, [id])
+
+	return (
+		<div>
+			<h5>Name: {name}</h5>
+			<h5>Created at: {createdAt}</h5>
+			<hr></hr>
+			<h5>Spel:</h5>
+			<ul>
+				{games.length ? (
+					games.map(game => <li>{game.name}</li>)
+				) : (
+					<li>Du har inte lagt till några spel ännu... :&lt;</li>
+				)}
+			</ul>
+		</div>
+	)
 }
 
 export default List
