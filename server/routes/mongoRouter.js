c53f4fef8de205d43e7a77cc0ff97756cc1fb5c9
ol222hf <ol222hf@student.lnu.se>
2020-05-11 12:24:46 +0200

    adding route for getting specific gamelist

--- server/routes/mongoRouter.js
+++ server/routes/mongoRouter.js
@@ -3,7 +3,11 @@ const router = express.Router()
 
 const controller = require('../controllers/mongoController')
 
+// Lists
 router.get('/lists', controller.getLists)
 router.post('/lists/new', controller.newList)
+
+// List
+router.get('/list/:id', controller.getList)
 
 module.exports = router
