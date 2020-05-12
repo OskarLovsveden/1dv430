ae708f2dc0ba04e1741d5831ea6038bd02462cca
ol222hf <ol222hf@student.lnu.se>
2020-05-12 17:46:41 +0200

    created app to render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import Layout from './layout/Layout'
+import { GameProvider } from './context/GameState'
 
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Layout />, document.getElementById('root'))
+const app = (
+	<GameProvider>
+		<Layout />
+	</GameProvider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
