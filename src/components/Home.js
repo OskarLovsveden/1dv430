bcd12022368b04e2d39927e419002c2cfef00a95
ol222hf <ol222hf@student.lnu.se>
2020-05-12 23:10:16 +0200

    Breaking out components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,8 @@
-import React, { useState } from 'react'
-
-// Components
-import SearchBar from '../components/SearchBar'
-import GameCard from './GameCard'
+import React from 'react'
+import SearchGame from './SearchGame'
 
 const Home = () => {
-	const [gameData, setGameData] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
-
-	const handleValue = value => {
-		setGameData(value)
-	}
-
-	const handleLoading = loading => {
-		setIsLoading(loading)
-	}
-
-	return (
-		<div>
-			<SearchBar
-				value={handleValue}
-				loading={handleLoading}
-				placeholder="Search for a game..."
-			></SearchBar>
-			{isLoading ? 'Loading' : null}
-			{gameData
-				? gameData.map(game => <GameCard key={game.id} game={game}></GameCard>)
-				: null}
-		</div>
-	)
+	return <SearchGame />
 }
 
 // Exports
