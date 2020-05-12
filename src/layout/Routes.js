8ab74dcd8d99781009313cd007245efcc38feaaa
ol222hf <ol222hf@student.lnu.se>
2020-05-12 17:45:42 +0200

    removing about and adding game route

--- src/layout/Routes.js
+++ src/layout/Routes.js
@@ -4,23 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Nav from '../components/Nav'
 import Home from '../components/Home'
 import Lists from '../components/Lists'
-import About from '../components/About'
 import NotFound from '../components/NotFound'
 import List from '../components/List'
+import Game from '../components/Game'
 
 const Routes = () => (
 	<Router>
 		<Nav />
 		<div>
 			<Switch>
+				<Route path="/game">
+					<Game />
+				</Route>
 				<Route path="/list/:id">
 					<List />
 				</Route>
 				<Route path="/lists">
 					<Lists />
-				</Route>
-				<Route path="/about">
-					<About />
 				</Route>
 				<Route exact path="/">
 					<Home />
