d673e14447dd9b30050f0e5fe79ddd04ec1add1d
ol222hf <ol222hf@student.lnu.se>
2020-05-12 23:23:46 +0200

    gets available lists on render

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import { GameContext } from '../context/GameState'
 
+import mongoHelper from '../helpers/mongoHelper'
+const { getLists } = mongoHelper
+
 const Game = () => {
+	const [lists, setLists] = useState([])
 	const { game } = useContext(GameContext)
-	console.log(game)
+
+	useEffect(() => {
+		const getListsOnRender = async () => {
+			const listsData = await getLists()
+			setLists(listsData)
+		}
+		getListsOnRender()
+	}, [])
 
 	if (!game) {
 		return <Redirect to="/"></Redirect>
