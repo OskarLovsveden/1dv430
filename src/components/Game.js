ac883f3d10427b805e08463e6082d1882e1f8f40
ol222hf <ol222hf@student.lnu.se>
2020-05-12 23:10:02 +0200

    Redirects user to Home if no gamecontext exists

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,21 @@
 import React, { useContext } from 'react'
+import { Redirect } from 'react-router-dom'
 import { GameContext } from '../context/GameState'
 
 const Game = () => {
 	const { game } = useContext(GameContext)
-	return <div>{game.name}</div>
+	console.log(game)
+
+	if (!game) {
+		return <Redirect to="/"></Redirect>
+	}
+
+	return (
+		<div>
+			{game.name}
+			<button>Add To List</button>
+		</div>
+	)
 }
 
 export default Game
