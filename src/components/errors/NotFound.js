01fef111539fe56936d354a4d6654c9115bb26d3
ol222hf <ol222hf@student.lnu.se>
2020-05-14 16:12:52 +0200

    updating html displayed on 404 errors

---
+++ src/components/errors/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+	return (
+		<div className="text-center">
+			<h1>404 Not Found</h1>
+			<p>Sorry, but the content you were trying to view does not exist.</p>
+		</div>
+	)
+}
+export default NotFound
