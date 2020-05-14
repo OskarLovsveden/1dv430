5c7a60640b413daad2a3bac1725bc11910c0a587
ol222hf <ol222hf@student.lnu.se>
2020-05-14 16:13:12 +0200

    adding proper validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,54 +1,134 @@
+// Inpiration for validation of form - https://www.telerik.com/blogs/up-and-running-with-react-form-validation
+
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Register = () => {
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
-	const [confirm, setConfirm] = useState('')
+	const [username, setUsername] = useState(null)
+	const [password, setPassword] = useState(null)
+	const [errors, setErrors] = useState({ password: '', confirm: '' })
+	const [flash, setFlash] = useState(null)
 
-	const handleUsername = event => {
-		setUsername(event.target.value)
+	const validateForm = () => {
+		let valid = true
+		Object.values(errors).forEach(error => {
+			if (error.length) valid = false
+		})
+		if (username === null || !username.length) {
+			valid = false
+		}
+		return valid
 	}
-	const handlePassword = event => {
-		setPassword(event.target.value)
-	}
-	const handleConfirm = event => {
-		setConfirm(event.target.value)
-	}
-	const registerUser = event => {
+
+	const handleChange = event => {
 		event.preventDefault()
+		const { name, value } = event.target
 
-		if (password !== confirm) {
-			console.log('Not Matching')
+		switch (name) {
+			case 'username':
+				setUsername(value)
+				break
+			case 'password':
+				setPassword(value)
+				setErrors({
+					...errors,
+					password:
+						value.length < 10 ? 'Password must be 10 characters long!' : ''
+				})
+				break
+			case 'confirm':
+				setErrors({
+					...errors,
+					confirm: value !== password ? 'Passwords must match!' : ''
+				})
+				break
+			default:
+				break
+		}
+	}
+
+	const registerUser = async event => {
+		event.preventDefault()
+		if (validateForm()) {
+			const response = await axios({
+				method: 'post',
+				url: 'user/register',
+				data: {
+					username: username,
+					password: password
+				}
+			})
+			setFlash(response.data)
 		} else {
-			console.log('Matching - Register user.')
+			console.error('Invalid Form')
 		}
 	}
 
 	return (
-		<form>
-			<input
-				placeholder="Username"
-				value={username}
-				onChange={handleUsername}
-				required={true}
-			></input>
-			<br></br>
-			<input
-				placeholder="Password"
-				value={password}
-				onChange={handlePassword}
-				required={true}
-			></input>
-			<br></br>
-			<input
-				placeholder="Confirm Password"
-				onChange={handleConfirm}
-				value={confirm}
-				required={true}
-			></input>
-			<br></br>
-			<button onClick={registerUser}>Register</button>
-		</form>
+		<>
+			{flash && (
+				<div className={`alert alert-${flash.type}`} role="alert">
+					{flash.text}
+				</div>
+			)}
+			<div className="d-flex justify-content-center pb-5">
+				<form className="text-center p-5">
+					<p className="h4 mb-4">Register</p>
+					<input
+						type="text"
+						name="username"
+						className="form-control mb-4"
+						placeholder="Username"
+						required="required"
+						autoFocus="autofocus"
+						onChange={handleChange}
+					></input>
+					<input
+						type="password"
+						name="password"
+						className="form-control mb-2"
+						placeholder="Password"
+						required="required"
+						aria-describedby="passwordHelpBlock"
+						onChange={handleChange}
+					></input>
+					{errors.password && (
+						<small
+							id="passwordHelpBlock"
+							className="form-text text-muted float-left mb-4"
+						>
+							{errors.password}
+						</small>
+					)}
+					<input
+						type="password"
+						name="confirm"
+						className="form-control mb-2"
+						placeholder="Confirm Password"
+						required="required"
+						aria-describedby="confirmHelpBlock"
+						onChange={handleChange}
+					></input>
+					{errors.confirm && (
+						<small
+							id="confirmHelpBlock"
+							className="form-text text-muted float-left mb-4"
+						>
+							{errors.confirm}
+						</small>
+					)}
+					<button
+						className="btn btn-primary btn-block my-4"
+						onClick={registerUser}
+					>
+						Register
+					</button>
+					<p>
+						Already registered? <a href="/login">Login</a>
+					</p>
+				</form>
+			</div>
+		</>
 	)
 }
 
