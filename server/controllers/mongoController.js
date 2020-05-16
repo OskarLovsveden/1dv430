20a9998eb84e86f1c86cc84f62ded84227a72bdb
ol222hf <ol222hf@student.lnu.se>
2020-05-17 01:52:29 +0200

    adding trycatches

--- server/controllers/mongoController.js
+++ server/controllers/mongoController.js
@@ -3,29 +3,63 @@ const List = require('../models/List')
 
 const mongoController = {
 	newList: async (req, res) => {
-		const listName = generateName()
-		const list = new List({ name: listName })
-		list.save()
-		res.json({ message: 'New list added.', list: list })
+		try {
+			const listName = generateName()
+			const list = new List({ name: listName })
+			list.save()
+
+			const message = {
+				type: 'success',
+				text: `New list: ${list.name}`
+			}
+
+			res.json({ message: message, list: list })
+		} catch (error) {
+			const message = {
+				type: 'danger',
+				text: `Failed to add list`
+			}
+			res.json(message)
+		}
 	},
 	getLists: async (req, res) => {
-		const lists = await List.find()
-		res.json(lists)
+		try {
+			const lists = await List.find()
+			res.json(lists)
+		} catch (error) {
+			console.error(error.message)
+		}
 	},
 	getList: async (req, res) => {
-		const id = req.params.id
-		const list = await List.findById({ _id: id })
-		res.json(list)
+		try {
+			const id = req.params.id
+			const list = await List.findById({ _id: id })
+			res.json(list)
+		} catch (error) {
+			console.error(error.message)
+		}
 	},
 	saveGame: async (req, res) => {
-		const id = req.params.listid
-		const data = req.body
+		try {
+			const id = req.params.listid
+			const data = req.body
 
-		const list = await List.findById({ _id: id })
-		list.games = [...list.games, data]
-		list.save()
+			const list = await List.findById({ _id: id })
+			list.games = [...list.games, data]
+			list.games = list.games.filter(
+				(game, index, self) => index === self.findIndex(g => g.id === game.id)
+			)
+			list.save()
 
-		res.json({ message: `Game added to ${list.name}` })
+			const message = {
+				type: 'success',
+				text: `Game added to ${list.name}`
+			}
+
+			res.json(message)
+		} catch (error) {
+			console.error(error.message)
+		}
 	}
 }
 module.exports = mongoController
