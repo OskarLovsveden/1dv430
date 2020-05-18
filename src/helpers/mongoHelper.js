52b31780150b366fc81ac9fae5851c665828862c
ol222hf <ol222hf@student.lnu.se>
2020-05-18 10:36:06 +0200

    Adding try catches

--- src/helpers/mongoHelper.js
+++ src/helpers/mongoHelper.js
@@ -2,27 +2,58 @@ import axios from 'axios'
 
 const mongoHelper = {
 	newList: async () => {
-		const response = await axios({
-			method: 'post',
-			url: 'mongo/list/new'
-		})
-		return response.data.list
+		try {
+			const response = await axios({
+				method: 'post',
+				url: 'mongo/list/new'
+			})
+			return response.data
+		} catch (error) {
+			console.error(error)
+		}
 	},
 	getLists: async () => {
-		const response = await axios('/mongo/lists')
-		return response.data
+		try {
+			const response = await axios('/mongo/lists')
+			return response.data
+		} catch (error) {
+			console.error(error.message)
+		}
 	},
 	getList: async id => {
-		const response = await axios(`/mongo/list/${id}`)
-		return response.data
+		try {
+			const response = await axios(`/mongo/list/${id}`)
+			return response.data
+		} catch (error) {
+			console.error(error.message)
+		}
 	},
 	saveGame: async (game, listID) => {
-		const response = await axios({
-			method: 'post',
-			url: `/mongo/save/${listID}`,
-			data: game
-		})
-		return response.data
+		try {
+			const response = await axios({
+				method: 'post',
+				url: `/mongo/save/${listID}`,
+				data: game
+			})
+			return response.data
+		} catch (error) {
+			console.error(error)
+		}
+	},
+	login: async (username, password) => {
+		try {
+			const response = await axios({
+				method: 'post',
+				url: 'user/login',
+				data: {
+					username: username,
+					password: password
+				}
+			})
+			return response.data
+		} catch (error) {
+			console.error(error.message)
+		}
 	}
 }
 
