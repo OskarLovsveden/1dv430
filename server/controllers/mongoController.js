28b740b5f9caf70c4524ee6bb043208441e21371
ol222hf <ol222hf@student.lnu.se>
2020-05-18 10:30:43 +0200

    restructure and adding check on if user is logged in

--- server/controllers/mongoController.js
+++ server/controllers/mongoController.js
@@ -1,65 +1,82 @@
 const { generateName } = require('../helpers/nameHelper')
 const List = require('../models/List')
 
-const mongoController = {
-	newList: async (req, res) => {
-		try {
-			const listName = generateName()
-			const list = new List({ name: listName })
-			list.save()
+const createError = require('http-errors')
 
-			const message = {
-				type: 'success',
-				text: `New list: ${list.name}`
-			}
+const mongoController = {}
 
-			res.json({ message: message, list: list })
-		} catch (error) {
-			const message = {
-				type: 'danger',
-				text: `Failed to add list`
-			}
-			res.json(message)
+// New List
+mongoController.newList = async (req, res) => {
+	try {
+		const listName = generateName()
+		const list = new List({ name: listName, author: req.session.user })
+		list.save()
+
+		const message = {
+			type: 'success',
+			text: `New list: ${list.name}`
 		}
-	},
-	getLists: async (req, res) => {
-		try {
-			const lists = await List.find()
-			res.json(lists)
-		} catch (error) {
-			console.error(error.message)
-		}
-	},
-	getList: async (req, res) => {
-		try {
-			const id = req.params.id
-			const list = await List.findById({ _id: id })
-			res.json(list)
-		} catch (error) {
-			console.error(error.message)
-		}
-	},
-	saveGame: async (req, res) => {
-		try {
-			const id = req.params.listid
-			const data = req.body
 
-			const list = await List.findById({ _id: id })
-			list.games = [...list.games, data]
-			list.games = list.games.filter(
-				(game, index, self) => index === self.findIndex(g => g.id === game.id)
-			)
-			list.save()
-
-			const message = {
-				type: 'success',
-				text: `Game added to ${list.name}`
-			}
-
-			res.json(message)
-		} catch (error) {
-			console.error(error.message)
+		res.json({ message: message, list: list })
+	} catch (error) {
+		const message = {
+			type: 'danger',
+			text: `Failed to add list`
 		}
+		res.json(message)
 	}
+}
+
+// Get all lists
+mongoController.getLists = async (req, res) => {
+	try {
+		const lists = await List.find()
+		res.json(lists)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
+// Get specific list of _id
+mongoController.getList = async (req, res) => {
+	try {
+		const id = req.params.id
+		const list = await List.findById({ _id: id })
+		res.json(list)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
+// Save game to list with given list _id
+mongoController.saveGame = async (req, res) => {
+	try {
+		const id = req.params.listid
+		const data = req.body
+
+		const list = await List.findById({ _id: id })
+		list.games = [...list.games, data]
+		list.games = list.games.filter(
+			(game, index, self) => index === self.findIndex(g => g.id === game.id)
+		)
+		list.save()
+
+		const message = {
+			type: 'success',
+			text: `Game added to ${list.name}`
+		}
+
+		res.json(message)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
+// Check if user is logged in
+mongoController.isUserLoggedIn = async (req, res, next) => {
+	if (!req.session.user) {
+		return next(createError(403, 'Forbidden access'))
+	}
+	next()
 }
 module.exports = mongoController
