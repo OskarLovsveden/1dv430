5e3bebcbc164b3d22471415ce4417fa33f2cad55
ol222hf <ol222hf@student.lnu.se>
2020-05-18 10:33:56 +0200

    backend calls moved back into component

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,8 @@
-import moment from 'moment'
-
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import mongoHelper from '../helpers/mongoHelper'
-const { getList } = mongoHelper
+import axios from 'axios'
+import moment from 'moment'
 
 const List = () => {
 	const [name, setName] = useState('')
@@ -15,10 +13,15 @@ const List = () => {
 
 	useEffect(() => {
 		const getListOnRender = async () => {
-			const list = await getList(id)
-			setName(list.name)
-			setGames(list.games)
-			setCreatedAt(moment(list.createdAt).format('YYYY-MM-DD'))
+			try {
+				const response = await axios(`/mongo/list/${id}`)
+				const data = response.data
+				setName(data.name)
+				setGames(data.games)
+				setCreatedAt(moment(data.createdAt).format('YYYY-MM-DD'))
+			} catch (error) {
+				console.error(error.message)
+			}
 		}
 		getListOnRender()
 	}, [id])
