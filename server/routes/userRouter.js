42adbe0dff349d332fca62772c25fe3af1ceb0d9
ol222hf <ol222hf@student.lnu.se>
2020-05-18 10:32:38 +0200

    adding route for getting active session

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const controller = require('../controllers/userController')
 
+// Check for active session
+router.get('/session', controller.session)
+
 // Register
 router.post('/register', controller.register)
 
