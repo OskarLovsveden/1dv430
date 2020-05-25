3365defbcbecf93d62559d58b2752abdddce3449
ol222hf <ol222hf@student.lnu.se>
2020-05-25 22:45:18 +0200

    styling searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -30,14 +30,20 @@ const SearchBar = ({ value, loading, placeholder }) => {
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				onKeyPress={handleKeypress}
-				onChange={handleChange}
-				placeholder={placeholder}
-			></input>
-		</form>
+		<div className="d-flex flex-row p-2 justify-content-center">
+			<form className="form-inline" onSubmit={handleSubmit}>
+				<input
+					className="form-control"
+					type="text"
+					onKeyPress={handleKeypress}
+					onChange={handleChange}
+					placeholder={placeholder}
+				></input>
+				<button type="submit" className="btn btn-sm btn-outline-primary">
+					Search
+				</button>
+			</form>
+		</div>
 	)
 }
 
