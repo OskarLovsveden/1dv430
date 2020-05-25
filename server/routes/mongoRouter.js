d68d9c2f6180373e79554f0b7a01b8a5a0b35cba
ol222hf <ol222hf@student.lnu.se>
2020-05-25 21:43:42 +0200

    adding update and delete routes

--- server/routes/mongoRouter.js
+++ server/routes/mongoRouter.js
@@ -5,10 +5,13 @@ const controller = require('../controllers/mongoController')
 
 // Lists
 router.get('/lists', controller.getLists)
+router.get('/lists/:author', controller.getLists)
 
 // List
 router.get('/list/:id', controller.getList)
 router.post('/list/new', controller.isUserLoggedIn, controller.newList)
+router.post('/list/update/:id', controller.authorizeUser, controller.updateList)
+router.post('/list/delete/:id', controller.authorizeUser, controller.deleteList)
 
 // Save game
 router.post('/save/:listid', controller.isUserLoggedIn, controller.saveGame)
