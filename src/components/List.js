037aa78b2a7befb1da5ca01cd29b2a1579e4ff26
ol222hf <ol222hf@student.lnu.se>
2020-05-25 21:17:37 +0200

    changed from useParams to useLocation

--- src/components/List.js
+++ src/components/List.js
@@ -1,49 +1,59 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
-import { useParams } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
+import DeleteListModal from './DeleteListModal'
 
-import axios from 'axios'
 import moment from 'moment'
 
 const List = () => {
-	const { id } = useParams()
+	const history = useHistory()
+	const location = useLocation()
+	const [list] = useState(location.state)
 	const { user } = useContext(GlobalContext)
 
-	const [list, setList] = useState(null)
-	const [editable, isEditable] = useState(false)
-
-	useEffect(() => {
-		const getListOnRender = async () => {
-			try {
-				const response = await axios(`/mongo/list/${id}`)
-				const data = response.data
-				setList(data)
-				isEditable(data.author === user)
-			} catch (error) {
-				console.error(error.message)
-			}
+	const editList = () => {
+		if (list.author === user) {
+			history.push({ pathname: '/list-edit', state: list })
 		}
-		getListOnRender()
-	}, [id, user])
+	}
 
 	return (
 		list && (
-			<div>
-				<h5>{list.name}</h5>
+			<div className="p-2">
+				<div>
+					<span className="h4">{list.name}</span>
+					{list.author === user && (
+						<>
+							<button
+								onClick={editList}
+								className="btn btn-sm btn-primary float-right"
+								type="button"
+							>
+								<i className="fas fa-cog"></i>
+							</button>
+							<DeleteListModal listID={list._id} />
+						</>
+					)}
+				</div>
 				<p className="text-muted">
 					Created at: {moment(list.createdAt).format('YYYY-MM-DD')}
 				</p>
+
 				<ul className="list-group">
 					{list.games.length ? (
 						list.games.map(game => (
 							<li className="list-group-item" key={game.id}>
-								{game.name}
-								{editable && (
-									<>
-										<button className="btn btn-sm mr2">Edit</button>
-										<button className="btn btn-sm mr2">Delete</button>
-									</>
-								)}
+								<span className="h5">{game.name}</span>
+								<br></br>
+								<input type="checkbox" checked={game.checked} disabled></input>
+								Collected
+								<br></br>
+								<input
+									type="checkbox"
+									checked={game.completed}
+									disabled
+								></input>
+								Completed
 							</li>
 						))
 					) : (
