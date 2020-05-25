98e80367994d2cf3e944398c6f83f300c92eb6ed
ol222hf <ol222hf@student.lnu.se>
2020-05-26 00:15:25 +0200

    flash component updated temporarly for production test

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
+// import React, { useContext, useEffect, useState } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+// import { cleanup } from '@testing-library/react'
 
-const Flash = props => {
-	const { type, text } = props
+const Flash = () => {
+	const { flash } = useContext(GlobalContext)
+	const { type, text } = flash
+
+	// const [state, setstate] = useState()
+
+	// useEffect(() => {
+	// 	setstate({ type: type, text: text })
+	// 	const visibility = setTimeout(() => {
+	// 		setstate(null)
+	// 	}, 5000)
+	// 	cleanup(clearTimeout(visibility))
+	// }, [type, text])
+
 	return (
-		<div className={`alert alert-${type}`} role="alert">
-			{text}
-		</div>
+		type.length !== 0 &&
+		text.length !== 0 && (
+			<div
+				className={`alert alert-${type} alert-dismissible fade show`}
+				role="alert"
+			>
+				{text}
+				<button
+					type="button"
+					className="close"
+					data-dismiss="alert"
+					aria-label="Close"
+				>
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		)
 	)
 }
 
-Flash.propTypes = {
-	props: PropTypes.shape({
-		type: PropTypes.string,
-		text: PropTypes.string
-	}).isRequired
-}
+export default Flash
