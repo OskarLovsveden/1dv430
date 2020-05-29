d4c1f507c837b1379f0a35088d39f42178e46e92
ol222hf <ol222hf@student.lnu.se>
2020-05-29 03:47:52 +0200

    removing flash functionality

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -2,21 +2,11 @@ export default (state, action) => {
 	switch (action.type) {
 		case 'USER_LOGIN':
 			return {
-				...state,
 				user: action.payload
 			}
 		case 'USER_LOGOUT':
 			return {
-				...state,
-				user: ''
-			}
-		case 'SET_FLASH':
-			return {
-				...state,
-				flash: {
-					type: action.payload.type,
-					text: action.payload.text
-				}
+				user: null
 			}
 		default:
 			return state
