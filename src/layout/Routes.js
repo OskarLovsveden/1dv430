7b434f60b56c3d286d8074833679b74fe42d4043
ol222hf <ol222hf@student.lnu.se>
2020-05-29 03:46:14 +0200

    custom route AuthRoute for authorization

--- src/layout/Routes.js
+++ src/layout/Routes.js
@@ -1,53 +1,51 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Home from '../components/Home'
-import Lists from '../components/Lists'
 import NotFound from '../components/errors/NotFound'
-import List from '../components/List'
+import Home from '../components/Home'
 import Game from '../components/Game'
+import Lists from '../components/Lists'
+import List from '../components/List'
+import ListEdit from '../components/ListEdit'
 import Register from '../components/account/Register'
 import Login from '../components/account/Login'
-import Flash from './Flash'
 import Logout from '../components/account/Logout'
-import ListEdit from '../components/ListEdit'
+import Flash from './Flash'
 
-const Routes = ({ children }) => (
-	<Router>
-		{children}
-		<div className="p-1 mb-5">
-			<Flash />
-			<Switch>
-				<Route path="/list-edit">
-					<ListEdit />
-				</Route>
-				<Route path="/register">
-					<Register />
-				</Route>
-				<Route path="/logout">
-					<Logout />
-				</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/game">
-					<Game />
-				</Route>
-				<Route path="/list">
-					<List />
-				</Route>
-				<Route path="/lists">
-					<Lists />
-				</Route>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/*">
-					<NotFound />
-				</Route>
-			</Switch>
-		</div>
-	</Router>
-)
+import AuthRoute from './AuthRoute'
+
+const Routes = ({ children }) => {
+	return (
+		<Router>
+			{children}
+			<div className="p-1 mb-5">
+				<Flash />
+				<Switch>
+					<Route path="/list-edit">
+						<ListEdit />
+					</Route>
+					<AuthRoute forLoggedIn={true} path="/register" component={Register} />
+					<AuthRoute forLoggedIn={true} path="/login" component={Login} />
+					<AuthRoute forLoggedIn={false} path="/logout" component={Logout} />
+					<Route path="/game">
+						<Game />
+					</Route>
+					<Route path="/list">
+						<List />
+					</Route>
+					<Route path="/lists">
+						<Lists />
+					</Route>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	)
+}
 
 export default Routes
