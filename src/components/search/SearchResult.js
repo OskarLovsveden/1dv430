27b5b2e3e5460e2fc58028cea622f7887da8bcf4
ol222hf <ol222hf@student.lnu.se>
2020-06-02 17:26:08 +0200

    adding proptypes

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import GameCard from './GameCard'
 
 const SearchResult = ({ gameData }) => {
@@ -8,6 +9,10 @@ const SearchResult = ({ gameData }) => {
 				gameData.map(game => <GameCard key={game.id} game={game}></GameCard>)}
 		</div>
 	)
+}
+
+SearchResult.propTypes = {
+	gameData: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired
 }
 
 export default SearchResult
