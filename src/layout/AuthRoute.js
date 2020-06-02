b54660133734a6165267ba34c006e2505493786c
ol222hf <ol222hf@student.lnu.se>
2020-06-02 17:26:15 +0200

    adding proptypes

--- src/layout/AuthRoute.js
+++ src/layout/AuthRoute.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { Route, Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const AuthRoute = ({ forLoggedIn, component: Component, ...rest }) => {
 	const { user } = useContext(GlobalContext)
@@ -23,6 +24,10 @@ const AuthRoute = ({ forLoggedIn, component: Component, ...rest }) => {
 			}
 		/>
 	)
+}
+
+AuthRoute.propTypes = {
+	forLoggedIn: PropTypes.bool.isRequired
 }
 
 export default AuthRoute
