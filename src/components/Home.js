d82e2aad02f5396c8bce76a3c3c600226d4210cf
ol222hf <ol222hf@student.lnu.se>
2020-06-02 16:37:00 +0200

    adding links to login/register on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,40 @@
 import React from 'react'
-import SearchGame from './SearchGame'
+import { useHistory } from 'react-router-dom'
+import SearchGame from '../components/search/SearchGame'
 
 const Home = () => {
-	return <SearchGame />
+	const history = useHistory()
+
+	return (
+		<div>
+			<SearchGame />
+			<div className="row row-cols-1 row-cols-md-2">
+				<div className="card m-auto text-center" style={{ maxWidth: '20rem' }}>
+					<div className="card-body">
+						<h3 className="card-title">Get Started!</h3>
+						<p className="card-text text-default">
+							Click a button to log into your account or register a new one!
+						</p>
+						<h5>Happy collecting!</h5>
+						<button
+							onClick={() => history.push('/login')}
+							type="button"
+							className="btn btn-outline-default btn-md"
+						>
+							Login
+						</button>
+						<button
+							onClick={() => history.push('/register')}
+							type="button"
+							className="btn  btn-outline-default btn-md"
+						>
+							Register
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-// Exports
 export default Home
