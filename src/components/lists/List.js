5c9db326b33294de45b7ed1d5b2303d09031e21d
ol222hf <ol222hf@student.lnu.se>
2020-06-03 22:11:04 +0200

    useEffect now sets list on render via location state

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -1,15 +1,24 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { GlobalContext } from '../../context/GlobalState'
 import { useHistory, useLocation } from 'react-router-dom'
 import DeleteListModal from './DeleteListModal'
 
 import moment from 'moment'
+import axios from 'axios'
 
 const List = () => {
 	const history = useHistory()
 	const location = useLocation()
-	const [list] = useState(location.state)
+	const [list, setList] = useState()
 	const { user } = useContext(GlobalContext)
+
+	useEffect(() => {
+		const getList = async () => {
+			const response = await axios(`mongo/list/${location.state}`)
+			setList(response.data)
+		}
+		getList()
+	}, [location.state])
 
 	const editList = () => {
 		if (list.author === user) {
@@ -17,51 +26,57 @@ const List = () => {
 		}
 	}
 
-	return (
-		list && (
-			<div className="p-2">
-				<div>
-					<span className="h4">{list.name}</span>
-					{list.author === user && (
-						<>
-							<button
-								onClick={editList}
-								className="btn btn-sm btn-outline-default float-right"
-								type="button"
-							>
-								<i className="fas fa-cog"></i>
-							</button>
-							<DeleteListModal listID={list._id} />
-						</>
-					)}
-				</div>
-				<p className="text-muted">
-					Created at: {moment(list.createdAt).format('YYYY-MM-DD')}
-				</p>
+	return list ? (
+		<div className="p-2">
+			<div>
+				<span className="h4">{list.name}</span>
+				{list.author === user && (
+					<>
+						<button
+							onClick={editList}
+							className="btn btn-sm btn-outline-default float-right"
+							type="button"
+						>
+							<i className="fas fa-edit"></i>
+						</button>
+						<DeleteListModal listID={list._id} />
+					</>
+				)}
+			</div>
+			<p className="text-muted m-0">Author: {list.author}</p>
+			<p className="text-muted m-0">
+				Created at: {moment(list.createdAt).format('YYYY-MM-DD')}
+			</p>
 
-				<ul className="list-group">
-					{list.games.length ? (
-						list.games.map(game => (
-							<li className="list-group-item" key={game.id}>
-								<span className="h5">{game.name}</span>
-								<br></br>
-								<input type="checkbox" checked={game.checked} disabled></input>
+			<ul className="list-group">
+				{list.games.length ? (
+					list.games.map(game => (
+						<li className="list-group-item" key={game.id}>
+							<h5>{game.name}</h5>
+							<div>
+								<input
+									type="checkbox"
+									checked={game.collected}
+									disabled
+								></input>{' '}
 								Collected
 								<br></br>
 								<input
 									type="checkbox"
 									checked={game.completed}
 									disabled
-								></input>
+								></input>{' '}
 								Completed
-							</li>
-						))
-					) : (
-						<li className="list-group-item">No games here... :&lt;</li>
-					)}
-				</ul>
-			</div>
-		)
+							</div>
+						</li>
+					))
+				) : (
+					<li className="list-group-item">No games here... :&lt;</li>
+				)}
+			</ul>
+		</div>
+	) : (
+		<></>
 	)
 }
 
