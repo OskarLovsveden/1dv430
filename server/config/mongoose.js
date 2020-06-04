ce30428d73437f1d557da5d6c3e0a3f38bffc69c
ol222hf <ol222hf@student.lnu.se>
2020-06-04 21:58:02 +0200

    different connectionstrings used for prod and dev

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 const config = require('./dotenv')
 const { DB_STRING } = config
 
+const CURR_DB_STRING =
+	process.env.NODE_ENV === 'production'
+		? DB_STRING
+		: 'mongodb+srv://testuser:RQYPgffO5MHFjk87@1dv430-i6ud6.mongodb.net/testing?retryWrites=true&w=majority'
+
 module.exports.connect = async () => {
 	mongoose.connection.on('connected', () =>
 		console.log('Mongoose connection is open.')
@@ -22,7 +27,7 @@ module.exports.connect = async () => {
 		})
 	})
 
-	return mongoose.connect(DB_STRING, {
+	return mongoose.connect(CURR_DB_STRING, {
 		useCreateIndex: true,
 		useNewUrlParser: true,
 		useUnifiedTopology: true
