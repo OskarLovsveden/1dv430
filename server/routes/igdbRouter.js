29215805ae1005e889df0995ba234c2689df96e3
ol222hf <ol222hf@student.lnu.se>
2020-06-07 17:40:16 +0200

    adding route for getting both games and their covers

--- server/routes/igdbRouter.js
+++ server/routes/igdbRouter.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const controller = require('../controllers/igdbController')
 
 router.get('/test', controller.test)
+router.get('/games-with-covers/:input', controller.gamesWithCovers)
 router.get('/games/:input', controller.games)
+router.get('/covers/:gameID', controller.covers)
 
 module.exports = router
