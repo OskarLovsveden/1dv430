602273be7364058463c4bc53f24b6f201a7cc85e
ol222hf <ol222hf@student.lnu.se>
2020-06-07 17:39:17 +0200

    adding method to return games with its cover image

--- server/controllers/igdbController.js
+++ server/controllers/igdbController.js
@@ -11,10 +11,55 @@ igdbController.test = async (req, res) => {
 	}
 }
 
+igdbController.gamesWithCovers = async (req, res) => {
+	try {
+		const input = req.params.input
+		const gamesWithoutCovers = await IGDB.getGames(input)
+
+		let covers = await Promise.all(
+			gamesWithoutCovers.map(async game => {
+				if (game.cover) {
+					return await IGDB.getCover(game.cover)
+				}
+			})
+		)
+
+		covers = covers.flat()
+
+		const data = []
+
+		gamesWithoutCovers.forEach(game => {
+			covers.forEach(cover => {
+				if (cover && game.id === cover.game) {
+					data.push({
+						...game,
+						coverUrl: cover.url,
+						coverImageID: cover.image_id
+					})
+				}
+			})
+		})
+
+		res.json(data)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
 igdbController.games = async (req, res) => {
 	try {
 		const input = req.params.input
 		const data = await IGDB.getGames(input)
+		res.json(data)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
+igdbController.covers = async (req, res) => {
+	try {
+		const gameID = req.params.gameID
+		const data = await IGDB.getCover(gameID)
 		res.json(data)
 	} catch (error) {
 		console.error(error.message)
