569d857580b69233f0a982f34c5aa0099208856f
ol222hf <ol222hf@student.lnu.se>
2020-06-07 19:40:06 +0200

    adding gamecover, summary and releasedate

--- src/components/search/GameCard.js
+++ src/components/search/GameCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 
 const GameCard = ({ game }) => {
 	const history = useHistory()
@@ -11,9 +12,26 @@ const GameCard = ({ game }) => {
 
 	return (
 		<div className="card mb-1">
-			<div className="card-body">
-				{game.name}
-				<br></br>
+			<div className="card-body d-flex align-items">
+				<div>
+					<img
+						className="mr-2"
+						src={`https:${game.coverUrl}`}
+						alt={`${game.slug}-cover`}
+					></img>
+				</div>
+				<div>
+					<div className="card-title">{game.name}</div>
+					<div className="card-text">{game.summary}</div>
+					<div className="card-text mt-2">
+						First released at:{' '}
+						{game.first_release_date
+							? moment.unix(game.first_release_date).format('YYYY-MM-DD')
+							: 'Unknown'}
+					</div>
+				</div>
+			</div>
+			<div className="card-footer">
 				<button
 					className="btn btn-sm btn-outline-default"
 					onClick={handleClick}
